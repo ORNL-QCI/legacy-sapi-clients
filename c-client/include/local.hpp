7a2a3be3301b930d7bb66d99a1f1fd5bfb93be4e
Alex McCaskey <mccaskeyaj@ornl.gov>
2019-12-09 13:20:40 -0500

    removing some of the orang solver stuff since we don't need it
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- c-client/include/local.hpp
+++ c-client/include/local.hpp
@@ -9,54 +9,68 @@
 
 namespace sapi {
 
-class LocalC4OptimizeSolver : public sapi_Solver {
-private:
-  virtual const sapi_SolverProperties* propertiesImpl() const;
-  virtual SubmittedProblemPtr submitImpl(
-      sapi_ProblemType type, const sapi_Problem *problem,
-      const sapi_SolverParameters *params) const;
+// class LocalC4OptimizeSolver : public sapi_Solver {
+// private:
+//   virtual const sapi_SolverProperties* propertiesImpl() const;
+//   virtual SubmittedProblemPtr submitImpl(
+//       sapi_ProblemType type, const sapi_Problem *problem,
+//       const sapi_SolverParameters *params) const;
 
-  virtual IsingResultPtr solveImpl(
-      sapi_ProblemType type, const sapi_Problem *problem,
-      const sapi_SolverParameters *params) const {
-    return submit(type, problem, params)->result();
-  }
-};
-
-
-class LocalC4SampleSolver : public sapi_Solver {
-private:
-  virtual const sapi_SolverProperties* propertiesImpl() const;
-  virtual SubmittedProblemPtr submitImpl(
-      sapi_ProblemType type, const sapi_Problem *problem,
-      const sapi_SolverParameters *params) const;
-
-  virtual IsingResultPtr solveImpl(
-      sapi_ProblemType type, const sapi_Problem *problem,
-      const sapi_SolverParameters *params) const {
-    return submit(type, problem, params)->result();
-  }
-};
+//   virtual IsingResultPtr solveImpl(
+//       sapi_ProblemType type, const sapi_Problem *problem,
+//       const sapi_SolverParameters *params) const {
+//     return submit(type, problem, params)->result();
+//   }
+// };
 
 
-class LocalIsingHeuristicSolver : public sapi_Solver {
-private:
-  virtual const sapi_SolverProperties* propertiesImpl() const;
-  virtual SubmittedProblemPtr submitImpl(
-      sapi_ProblemType type, const sapi_Problem *problem,
-      const sapi_SolverParameters *params) const;
+// class LocalC4SampleSolver : public sapi_Solver {
+// private:
+//   virtual const sapi_SolverProperties* propertiesImpl() const;
+//   virtual SubmittedProblemPtr submitImpl(
+//       sapi_ProblemType type, const sapi_Problem *problem,
+//       const sapi_SolverParameters *params) const;
 
-  virtual IsingResultPtr solveImpl(
-      sapi_ProblemType type, const sapi_Problem *problem,
-      const sapi_SolverParameters *params) const {
-    return submit(type, problem, params)->result();
-  }
-};
+//   virtual IsingResultPtr solveImpl(
+//       sapi_ProblemType type, const sapi_Problem *problem,
+//       const sapi_SolverParameters *params) const {
+//     return submit(type, problem, params)->result();
+//   }
+// };
 
+
+// class LocalIsingHeuristicSolver : public sapi_Solver {
+// private:
+//   virtual const sapi_SolverProperties* propertiesImpl() const;
+//   virtual SubmittedProblemPtr submitImpl(
+//       sapi_ProblemType type, const sapi_Problem *problem,
+//       const sapi_SolverParameters *params) const;
+
+//   virtual IsingResultPtr solveImpl(
+//       sapi_ProblemType type, const sapi_Problem *problem,
+//       const sapi_SolverParameters *params) const {
+//     return submit(type, problem, params)->result();
+//   }
+// };
+
+
+// namespace solvernames {
+// const auto sample = "c4-sw_sample";
+// const auto optimize = "c4-sw_optimize";
+// const auto heuristic = "ising-heuristic";
+// } // namespace solvernames
+
+sapi::SolverMap localSolverMap() {
+  auto ret = sapi::SolverMap();
+//   ret[solvernames::sample] = SolverPtr(new LocalC4SampleSolver);
+//   ret[solvernames::optimize] = SolverPtr(new LocalC4OptimizeSolver);
+//   ret[solvernames::heuristic] = SolverPtr(new LocalIsingHeuristicSolver);
+  return ret;
+}
 
 class LocalConnection : public sapi_Connection {
 public:
-  LocalConnection();
+  LocalConnection() :sapi_Connection(localSolverMap()) {}
 };
 
 } // namespace sapi
